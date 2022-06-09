f38b7673cf19347807b3a5dc4c0fcdf409f8f305
jazeem-khan <jazeem.khan@acentura.com>
2022-06-09 11:12:15 +0530

    fiegn maven commit

--- feign/src/main/java/com/example/feign/TestController.java
+++ feign/src/main/java/com/example/feign/TestController.java
@@ -3,4 +3,7 @@ package com.example.feign;
 public class TestController {
 
     public static String abc_xyz = "ABC_XYZ";
+
+    public static String PASSWORD = "123456";
+    public static String USERNAME = "Jazeem@allianz.lk";
 }
